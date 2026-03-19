48de1beeb119902fbaf5d27c589e8f28d416431b
Taffin Tom <taffin.tom59@gmail.com>
2026-03-19 22:36:17 +0100

    amélioration 11: correction du nom de la constante HOIZONTAL_DIRECTION dans la classe TextFigure

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/figure/TextFigure.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/figure/TextFigure.java
@@ -48,7 +48,7 @@ public class TextFigure extends AbstractAttributedDecoratedFigure
   private static final long serialVersionUID = 1L;
   protected Point2D.Double origin = new Point2D.Double();
 
-  public static final Point2D.Double HOIZONTAL_DIRECTION = new Point2D.Double(1, 0);
+  public static final Point2D.Double HORIZONTAL_DIRECTION = new Point2D.Double(1, 0);
 
   // always starting from 0,0
   protected Point2D.Double direction = new Point2D.Double(1, 0);
@@ -131,7 +131,7 @@ public class TextFigure extends AbstractAttributedDecoratedFigure
 
   @Override
   public void setAngle(double angle) {
-    AffineTransform.getRotateInstance(angle).transform(HOIZONTAL_DIRECTION, direction);
+    AffineTransform.getRotateInstance(angle).transform(HORIZONTAL_DIRECTION, direction);
   }
 
   @Override
